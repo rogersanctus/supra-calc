9a5cca77429e53d455c61af85191b7cea33c76d2
Rogério Santos <rogersanctus@gmail.com>
2023-03-15 15:13:56 -0300

    feat: adds css classes and testid attribute to the Display component

--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -5,10 +5,10 @@ interface DisplayProps {
 
 export function Display({ status = '', value }: DisplayProps) {
   return (
-    <div>
-      <div className="flex items-center bg-slate-900 border border-slate-400 text-white text-right p-4 text-2xl">
-        <span>{status}</span>
-        <span className="ml-auto font-bold">{value}</span>
+    <div data-testid="display">
+      <div className="display__box flex items-center bg-slate-900 border border-slate-400 text-white text-right p-4 text-2xl">
+        <span className="display__status">{status}</span>
+        <span className="display__value ml-auto font-bold">{value}</span>
       </div>
     </div>
   )
